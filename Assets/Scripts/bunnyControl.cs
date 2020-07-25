aaeab7d4d677857f91d675972f09a73992619e81
phillip <noReply@github>
2020-07-25 16:26:14 +1200

    added bunny run and jump animations

--- Assets/Scripts/bunnyControl.cs
+++ Assets/Scripts/bunnyControl.cs
@@ -7,12 +7,14 @@ public class bunnyControl : MonoBehaviour
 {
 
     private Rigidbody2D myRigidbody;
+    private Animator myAnimation;
     public float bunnyJumpForce = 500f;
 
     // Start is called before the first frame update
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
+        myAnimation = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -22,5 +24,9 @@ public class bunnyControl : MonoBehaviour
         {
             myRigidbody.AddForce(transform.up * bunnyJumpForce);
         }
+        // running animation starts as bunny is falling
+        myAnimation.SetFloat("vVelocity", myRigidbody.velocity.y);
+        // jumping animation maintains until bunny lands
+        // myAnimation.SetFloat("vVelocity", Mathf.Abs(myRigidbody.velocity.y));
     }
 }

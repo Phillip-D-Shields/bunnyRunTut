c6f70a22f2a0a297639826ec1962d7292c7b6ebf
phillip <noReply@github>
2020-07-25 15:49:29 +1200

    files moved, unity settings configured for version control

---
+++ Assets/Scripts/bunnyControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class bunnyControl : MonoBehaviour
+{
+
+    private Rigidbody2D myRigidbody;
+    public float bunnyJumpForce = 500f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonUp("Jump"))
+        {
+            myRigidbody.AddForce(transform.up * bunnyJumpForce);
+        }
+    }
+}

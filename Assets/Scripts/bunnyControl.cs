7c6bde2cf229e1ff6190c227933771ee8a8c8de3
phillip <noReply@github>
2020-07-26 22:57:01 +1200

    score logic and game over animations added

--- Assets/Scripts/bunnyControl.cs
+++ Assets/Scripts/bunnyControl.cs
@@ -2,44 +2,83 @@
 using System.Collections.Generic;
 using System.Security.Cryptography;
 using UnityEngine;
+// * needed for UI text box
+using UnityEngine.UI;
 
 public class bunnyControl : MonoBehaviour
 {
-
     private Rigidbody2D myRigidbody;
     private Animator myAnimation;
+    private Collider2D myCollider;
+    public Text scoreText;
     public float bunnyJumpForce = 500f;
+    private float bunnyHurtTime = -1;
+    private float startTime;
+    
 
-    // Start is called before the first frame update
+    //* Start is called before the first frame update
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
         myAnimation = GetComponent<Animator>();
+        myCollider = GetComponent<Collider2D>();
+
+        startTime = Time.time;
     }
 
-    // Update is called once per frame
+    //* Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonUp("Jump"))
+        //! bunny is running
+        if (bunnyHurtTime == -1)
         {
-            myRigidbody.AddForce(transform.up * bunnyJumpForce);
+            //* bunny wants to jump
+            if (Input.GetButtonUp("Jump"))
+            {
+                myRigidbody.AddForce(transform.up * bunnyJumpForce);
+            }
+            //* running animation starts as bunny is falling
+            myAnimation.SetFloat("vVelocity", myRigidbody.velocity.y);
+            //* jumping animation maintains until bunny lands
+            //* myAnimation.SetFloat("vVelocity", Mathf.Abs(myRigidbody.velocity.y));
+            scoreText.text = (Time.time - startTime).ToString("0.0");
         }
-        // running animation starts as bunny is falling
-        myAnimation.SetFloat("vVelocity", myRigidbody.velocity.y);
-        // jumping animation maintains until bunny lands
-        // myAnimation.SetFloat("vVelocity", Mathf.Abs(myRigidbody.velocity.y));
+        else
+        {
+            // ! bunny has collided
+            if (Time.time > bunnyHurtTime + 3)
+            {
+                Application.LoadLevel(Application.loadedLevel);
+            }
+        }
     }
 
-    /// <summary>
-    /// Sent when an incoming collider makes contact with this object's
-    /// collider (2D physics only).
-    /// </summary>
-    /// <param name="other">The Collision2D data associated with this collision.</param>
     void OnCollisionEnter2D(Collision2D collision)
     {
+        //! set collider only for items in the Enemy Layer
         if (collision.collider.gameObject.layer == LayerMask.NameToLayer("Enemy"))
         {
-            Application.LoadLevel(Application.loadedLevel);
+            // ! stop the prefab spawner
+            foreach (PrefabSpawn spawnPoint in FindObjectsOfType<PrefabSpawn>())
+            {
+                spawnPoint.enabled = false;
+            }
+            // ! stop the prefabs already spawned
+            foreach (MoveLeft MoveLefter in FindObjectsOfType<MoveLeft>())
+            {
+                MoveLefter.enabled = false;
+            }
+
+            // ! change the bunnyHurtTime to trigger final events
+            bunnyHurtTime = Time.time;
+            // * trigger bunnyHurt animation
+            myAnimation.SetBool("bunnyHurt", true);
+            // * stop the bunnys movement and velocity
+            myRigidbody.velocity = Vector2.zero;
+            // * trigger one last jump and fall off the screen
+            myRigidbody.AddForce(transform.up * bunnyJumpForce);
+            // * remove collision from bunny and ground
+            myCollider.enabled = false;
         }
     }
 }
